bae88c4d72f86ea42a4e2721b6e0ae048c9eb74a
rinka0v0 <r_moteki@yumemi.co.jp>
2024-05-13 16:30:57 +0900

    feat: todoタスクの追加機能を作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,53 @@
 import "./App.css";
 import { Button } from "@/components/ui/button";
 import { Amplify } from "aws-amplify";
-import { config } from "../aws-exports";
+import outputs from "../amplify_outputs.json";
 import { generateClient } from "aws-amplify/api";
-import { listTodos } from "@/graphql/queries";
+import type { Schema } from "../amplify/data/resource";
+import { useEffect, useState } from "react";
+import { Input } from "./components/ui/input";
 
-const client = generateClient();
-
-const test = async () => {
-  const data = await client.graphql({
-    query: listTodos,
-  });
-  return data;
-};
-
-Amplify.configure(config);
+Amplify.configure(outputs);
+const client = generateClient<Schema>();
 
 const App = () => {
-  const clickButton = async () => {
-    const data = await test();
-    console.log(data);
+  const [data, setData] = useState<Schema["Todo"]["type"][]>();
+  const [taskName, setTaskName] = useState<string>("");
+
+  const newTodo = async (taskName: string) => {
+    if (!taskName) return;
+    setTaskName("");
+
+    await client.models.Todo.create({
+      name: taskName,
+      when: "night",
+      where: "japan",
+      description: "テストタスク",
+    });
   };
 
-  return <Button onClick={clickButton}>test App</Button>;
+  useEffect(() => {
+    const sub = client.models.Todo.observeQuery().subscribe({
+      next: ({ items }) => {
+        setData([...items]);
+      },
+    });
+
+    return () => sub.unsubscribe();
+  }, []);
+
+  return (
+    <>
+      <div className="mb-12">
+        <Input value={taskName} onChange={(e) => setTaskName(e.target.value)} />
+        <Button onClick={() => newTodo(taskName)}>create Todo</Button>
+      </div>
+
+      <h1 className="text-3xl bold mb-6">タスク一覧</h1>
+
+      {data && data.map((item) => <div key={item?.id}>{item?.name}</div>)}
+    </>
+  );
 };
 
 export default App;
